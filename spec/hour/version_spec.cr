dd6251b64621f9a576eaa3eb8003a648727781e8
James C Russell <james@101ideas.cz>
2018-06-11 21:07:52 +0200

    Hour::VERSION defined dynamically from the shard.yml

---
+++ spec/hour/version_spec.cr
@@ -0,0 +1,10 @@
+require "../spec_helper"
+
+describe Hour do
+  it "is semantically versioned" do
+    Hour::VERSION.should eq("0.1.0")
+    # TODO: 11/06/2018 It says it's not matching.
+    # Hour::VERSION.should match(/^\d+\.\d+\.\d+$/)
+    # /^\d+\.\d+\.\d+$/.should match(Hour::VERSION)
+  end
+end
--- src/hour/version.cr
+++ src/hour/version.cr
@@ -1,3 +1,9 @@
-module Hour
-  VERSION = "0.1.0"
+require "yaml"
+
+class Hour
+  RAW_PKG_DATA = {% begin %}"{{system("cat shard.yml")}}"{% end %}
+  #PACKAGE = NamedTuple(String, String | Array(String)).new(YAML.parse(RAW_PKG_DATA))
+  #VERSION = PACKAGE[:version]
+  PACKAGE = YAML.parse(RAW_PKG_DATA)
+  VERSION = PACKAGE["version"]
 end
